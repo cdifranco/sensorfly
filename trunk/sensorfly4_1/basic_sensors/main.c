7fdc9cad2092e70a5453380ebb85e3011a7dbd9c
imaveek <imaveek@56807d80-2264-11df-ad4f-b38857f6a581>
2010-03-08 23:57:59 +0000

    Added Flight Controller Task and Drive Firmware

--- trunk/sensorfly4_1/basic_sensors/main.c
+++ trunk/sensorfly4_1/basic_sensors/main.c
@@ -1,33 +1,56 @@
-/*============================================================================
-  main.c
-*===========================================================================*/
+/******************************************************************************
+*  SensorFly, a controlled-mobile sensor networking platform.
+*  Copyright (C) 2010, Carnegie Mellon University
+*  All rights reserved.
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, version 2.0 of the License.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*******************************************************************************/
 
 #include <stdio.h>
 #include <string.h>
-#include "../common/LPC214x.h"
-#include "../TNKernel/tn.h"
-#include "../TNKernel/tn_port.h"
+#include "../common/sensorfly.h"
+#include "../SF_FlightController/sf_flightcontroller.h"
 
-#define LED_SEL 0x800000
+#define LED_SEL 0x800000 
 
-//----------- Tasks ----------------------------------------------------------
+//-----------------------------------------------------------------------------
+// Local declarations
+//-----------------------------------------------------------------------------
+#define  TASK_APP_PRIORITY   5
+#define  TASK_APP_STK_SIZE  128
 
-#define  TASK_PROCESSING_PRIORITY   5
-#define  TASK_PROCESSING_STK_SIZE  128
-unsigned int task_processing_stack[TASK_PROCESSING_STK_SIZE];
-TN_TCB  task_processing;
-void task_processing_func(void * par);
+unsigned int task_app_stack[TASK_APP_STK_SIZE];
+TN_TCB  task_app;
+void task_app_func(void * par);
 
+//-----------------------------------------------------------------------------
+// Definitions
+//-----------------------------------------------------------------------------
 
-//----------------------------------------------------------------------------
+/*! \fn int main(void)
+    \brief Main Function. Initializes hardware, starts kernel.
+    \param none
+    \return none
+*/
 int main(void)
 {
    hardware_init();
 
    // Set IO directions
-   rIO0DIR |= 0x800000;	
-   rIO0CLR |= 0x800000;	
-   rIO1DIR |= 0x200000;	
+   rIO0DIR |= 0x800000;	
+   rIO0CLR |= 0x800000;
+   rIO1DIR |= 0x200000;
    rIO1CLR |= 0x200000;
 
    tn_start_system(); //-- Never returns
@@ -35,26 +58,35 @@ int main(void)
    return 1;
 }
 
-//----------------------------------------------------------------------------
+/*! \fn void  tn_app_init()
+    \brief Initialize all tasks for the application
+    \param none
+    \return none
+*/
 void  tn_app_init()
 {
+   //--- Task flight APP
+   sf_flightcontroller_task_init();
 
-   //--- Task processing
-
-   task_processing.id_task = 0;
-   tn_task_create(&task_processing,            //-- task TCB
-                 task_processing_func,           //-- task function
-                 TASK_PROCESSING_PRIORITY,       //-- task priority
-                 &(task_processing_stack         //-- task stack first addr in memory
-                    [TASK_PROCESSING_STK_SIZE-1]),
-                 TASK_PROCESSING_STK_SIZE,       //-- task stack size (in int,not bytes)
+   //--- Task application
+   task_app.id_task = 1;
+   tn_task_create(&task_app,            //-- task TCB
+                 task_app_func,           //-- task function
+                 TASK_APP_PRIORITY,       //-- task priority
+                 &(task_app_stack         //-- task stack first addr in memory
+                    [TASK_APP_STK_SIZE-1]),
+                 TASK_APP_STK_SIZE,       //-- task stack size (in int,not bytes)
                  NULL,                           //-- task function parameter
                  TN_TASK_START_ON_CREATION     //-- Creation option
                  );
-
  }
-//----------------------------------------------------------------------------
-void task_processing_func(void * par)
+
+/*! \fn void task_app_func(void * par)
+    \brief The application task logic 
+    \param par Void pointer
+    \return none
+*/
+void task_app_func(void * par)
 {
    unsigned short Blink = 1;
 
@@ -79,7 +111,3 @@ void task_processing_func(void * par)
       tn_task_sleep(100);
    }
 }
-//----------------------------------------------------------------------------
-
-
-

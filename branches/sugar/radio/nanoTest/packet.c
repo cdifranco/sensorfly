f56e872daf83b94411aec38f6e258fdf82284b17
panshijiaustc <panshijiaustc@56807d80-2264-11df-ad4f-b38857f6a581>
2010-10-05 22:29:18 +0000

    can send Packet and receive from the other ARM side

--- branches/sugar/radio/nanoTest/packet.c
+++ branches/sugar/radio/nanoTest/packet.c
@@ -8,26 +8,6 @@
 #include <stdlib.h>
 #include "packet.h"
 
-
-Packet *CreateTestingPacket(void)
-{
-		Packet *pkt = (Packet *)malloc(sizeof(Packet));
-		
-		// 
-		PByte8T temp[2]	= {0,1};	
-		pkt->id = 1;
-		memcpy(pkt->dest, temp, 2);
-		temp[1]= 2;		
-		memcpy(pkt->src, temp, 2);
-		pkt->type = 'd';
-
-		//
-		PByte8T temp2[6] = "aaa~";
-		memcpy(pkt->data, temp2, 6);
-		pkt->checksum = 0;		
-		return pkt;
-}
-
 void PrintPacket(Packet *pkt)
 {
 		int i;
@@ -35,19 +15,17 @@ void PrintPacket(Packet *pkt)
 		printf("destination: ", pkt->dest);
 		for(i = 0; i < 6; i++)
 		{
-				if (i == 5) printf(" %x \n",pkt->dest[1]);
-				else if(i == 4) printf(" %x :",pkt->dest[0]);		
-				else printf(" %x :",0);
+				if (i == 5) printf(" %d \n",pkt->dest);
+				else printf(" %d :",0);
 		}
 		printf("source: ", pkt->src);
 		for(i = 0; i < 6; i++)
 		{
-				if (i == 5) printf(" %x \n",pkt->src[1]);
-				else if(i == 4) printf(" %x :",pkt->src[0]);		
-				else printf(" %x :",0);		
+				if (i == 5) printf(" %d \n",pkt->src);
+				else printf(" %d :",0);
 		}
 		printf("type: %c \n", pkt->type);
-		printf("data: %s \n", pkt->data);
+		printf("length: %d \n", pkt->length);
 		printf("checksum: %d \n", pkt->checksum);
 		
 }	

b549c4755bae6ab8844f06386de4e1f038e08a83
ckfbernard@gmail.com <ckfbernard@gmail.com@56807d80-2264-11df-ad4f-b38857f6a581>
2011-01-20 09:56:34 +0000

    develop navigation part of maemo app

--- branches/sugar/maemo_app/mainwindow.h
+++ branches/sugar/maemo_app/mainwindow.h
@@ -3,7 +3,7 @@
 
 #include <QtGui/QMainWindow>
 
-class Packet;
+//class Packet;
 class QBuffer;
 class QString;
 class QStackedWidget;
@@ -11,6 +11,8 @@ class QWidget;
 class QLabel;
 class QMenu;
 class QAction;
+class QLineEdit;
+class QPushButton;
 
 class MainWindow : public QMainWindow
 {
@@ -24,9 +26,11 @@ protected:
 
 private slots:
     void about(void) const;
-    void sendData(void) const;
-
     void btChangeDone(QString btAddr, QString btName, QString btIcon, QString majorClass, QString minorClass, bool trusted, QStringList services);
+    void enableGoButton(const QString &text) const;
+    void naviBegin(void) const;
+    void naviCanceled(void) const;
+    void arrived(void) const;
 
 private:
     /*****Methods*****/
@@ -36,11 +40,18 @@ private:
     void createNaviWidget(void);
     void createCtrlWidget(void);
 
-    void initBluetoothConn(void);
+    void createNaviDirect(void);
+    void createNaviDest(void);
+    void createNaviSubWidget(void);
+
+    /*void initBluetoothConn(void);
     void setupBluetoothConn(void);
-    void changeBluetoothConn(void);
-    void sendPkt(Packet *buf) const;
-    Packet *resvPkt() const;
+    void changeBluetoothConn(void);*/
+    void initNetConn(void);
+    void setupNetConn(void);
+    void changeNetConn(void);
+    //void sendPkt(Packet *buf) const;
+    //Packet *resvPkt() const;
 
     /******Static Constant*****/
     static const QString* const btDbusService;
@@ -76,12 +87,26 @@ private:
     QWidget *naviSubWidget;
     QWidget *ctrlSubWidget;
 
+    QStackedWidget *naviDirect;
+    QWidget *naviDest;
+    QLineEdit *destEdit;
+    QPushButton *goButton;
+    QPushButton *cancelButton;
+    QPushButton *arriButton;
+
     int mainIndex;
     int naviIndex;
     int ctrlIndex;
 
+    int forwardIndex;
+    int backIndex;
+    int leftIndex;
+    int rightIndex;
+
     QString *sfbtAddr;  //Sensorfly Bluetooth Address
     int btSocket;
+    QString *sfnetAddr;  //Sensorfly Network Address
+    int netSocket;
 };
 
 #endif // MAINWINDOW_H

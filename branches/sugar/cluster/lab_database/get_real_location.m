91cd3675609f963c11de3e05b7f16d9f92740e9b
panshijiaustc@gmail.com <panshijiaustc@gmail.com@56807d80-2264-11df-ad4f-b38857f6a581>
2011-01-27 20:18:02 +0000

    missed out

---
+++ branches/sugar/cluster/lab_database/get_real_location.m
@@ -0,0 +1,22 @@
+% record real location of the signature (used only for stage 4 experiment)
+function [x y] = get_real_location(old_x, old_y)
+%%
+while 1
+    cmd = input('command: 1--add x; 2--add y; 3--change x');
+    if cmd == 1 || cmd == 2 || cmd == 3
+        break;
+    end
+end
+%%
+if cmd == 1
+    x = old_x +10;
+    y = old_y;
+elseif cmd == 2 
+    y = old_y +10;
+    newx = input('new x: ');
+    x = newx;
+else 
+    newx = input('new x: ');
+    x = newx;
+    y = old_y;
+end

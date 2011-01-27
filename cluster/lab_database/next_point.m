0b4d34dbfaa64f7e25230b056946dafc18b559c6
panshijiaustc <panshijiaustc@gmail.com>
2011-01-27 21:47:11 +0000

    structure of database plan

---
+++ cluster/lab_database/next_point.m
@@ -0,0 +1,9 @@
+function [next_point dir] = next_point(current_point, signature)
+next_point = [-1, -1];
+current_x = current_point(1);
+current_y = current_point(2);
+isvalid_sig = 0;
+
+x_change = 
+
+

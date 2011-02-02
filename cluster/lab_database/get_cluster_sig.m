b6ba1dd6faea751ca65dc520eda384d9d488facd
panshijiaustc <panshijiaustc@gmail.com>
2011-02-02 21:35:33 +0000

    with kmeans testing and random testing

---
+++ cluster/lab_database/get_cluster_sig.m
@@ -0,0 +1,8 @@
+% get the cluster which the signature belongs to 
+function cluster = get_cluster_sig(centers, sig)
+dist = zeros(1, size(centers,1));
+for i = 1:size(centers,1)
+    center_sig = centers(i,:);
+    dist(i) = sum((sig - center_sig).^2).^.5;
+end
+[~, cluster] = min(dist);

69ee096c2b59391e52f4763f6183e413f21d95b1
frankmokaya <frankmokaya@56807d80-2264-11df-ad4f-b38857f6a581>
2011-02-07 19:49:19 +0000

    sonic.c updated

--- sensorfly4_1/SF_Firmware/SF_Sensors/sf_sonic.c
+++ sensorfly4_1/SF_Firmware/SF_Sensors/sf_sonic.c
@@ -1,21 +1,21 @@
 /******************************************************************************
-*  SensorFly, a controlled-mobile sensor networking platform.
-*  Copyright (C) 2010, Carnegie Mellon University
-*  All rights reserved.
-*
-*  This program is free software: you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation, version 2.0 of the License.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*******************************************************************************/
+ *  SensorFly, a controlled-mobile sensor networking platform.
+ *  Copyright (C) 2010, Carnegie Mellon University
+ *  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 2.0 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *******************************************************************************/
 
 #include "../../common/sensorfly.h"
 #include "sf_sonic.h"
@@ -30,10 +30,10 @@
 // Definitions
 //-----------------------------------------------------------------------------
 /*! \fn void sf_sensor_sonic_init(void)
-    \brief Initialize the ultrasound height sensor
-    \param none
-    \return none
-*/
+ \brief Initialize the ultrasound height sensor
+ \param none
+ \return none
+ */
 void sf_sensor_sonic_init() 
 {
     // Enable ADC1.7
@@ -41,27 +41,40 @@ void sf_sensor_sonic_init()
 }
 
 /*! \fn uint32_t sf_sensor_sonic_get() 
-    \brief Get height reading from ultrasound sensor
-    \param none
-    \return The value
-*/
+ \brief Get height reading from ultrasound sensor
+ \param none
+ \return The value
+ */
 uint32_t sf_sensor_sonic_get() 
 {
-
+	
     volatile uint32_t val = 0;
-
+	
     rAD1CR = 0x00200400 | (1 << 7);
-
+	
     rAD1CR |= 0x01000000;                              // START = 001 = Start Conversion Now
-
+	
     do
     {
-      val = rAD1GDR;                                   // Read A/D Data Register
+		val = rAD1GDR;                                   // Read A/D Data Register
     }
     while ((val & 0x80000000) == 0);                  // Wait ADC Conversion Complete
     val = (val >> 6) & 0x03FF;                        // Shift ADC Result to Integer
-
+	
     rAD1CR = 0x00;
-
+	
     return val;
 }
+/* This function converts the read ADC value into inches
+ * ADC1.7 is a 10 bit ADC pegged at 3.3V
+ * a reading of '1' corresponds to 3.3/1023 V
+ * Data sheet specifies that sonic sensor in analog mode yields:
+ * 0.0064V per inch
+ * dist in inches = (reading*(3.3/1023))/0.0064
+ */
+uint32_t sf_sensor_sonic_to_inches(uint32_t adcValue)
+{
+	volatile uint32_t val_to_inches = 0;
+	val_to_inches = (uint32_t)((adcValue * (3.3/1023))/ 0.0064);
+   return val_to_inches;
+ }

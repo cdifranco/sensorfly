e611b61f9f9355013f3a20b1867c01b30d9ba73a
panshijiaustc <panshijiaustc@gmail.com>
2011-02-08 03:22:04 +0000

    step 2 modified

--- cluster/supermarket_exp/step2/get_sig_from_port.m
+++ cluster/supermarket_exp/step2/get_sig_from_port.m
@@ -1,10 +1,10 @@
 % use serial port to get signature from anchors
-function [sig packet_id] = get_sig_from_port(packet_id, node_id, serial_port, base_number)
+function [sig packet_id] = get_sig_from_port(packet_id, serial_port, base_number)
 sig = zeros(1, base_number);
 for anchor_id = 1:base_number
     %% generate ranging packet
     packet_id = mod(packet_id + 1, 255);
-    msg_array = [uint8(packet_id),uint8('g'),uint8(0),uint8(node_id),uint8(1),uint8(24), typecast(uint16(anchor_id+2),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(single(0.0),'uint8'), typecast(single(0.0),'uint8')];
+    msg_array = [uint8(packet_id),uint8('g'),uint8(0),uint8(anchor_id+2),uint8(1),uint8(24), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(single(0.0),'uint8'), typecast(single(0.0),'uint8')];
     % id, type, checksum, dest, src, length, data_int[5], data_float[2]
     msg_new = [uint8(255)];
     for i = 1 : length(msg_array)
@@ -28,7 +28,7 @@ for anchor_id = 1:base_number
         try
             %% reading from serial port
             rx_pkt_info = fscanf(serial_port);
-            temp_double = textscan(rx_pkt_info, '%f, %f');
+            temp_double = textscan(rx_pkt_info, '%d, %c, %d, %d, %d, %d, %d, %d, %d, %f, %f');
             data_double = temp_double{1};
             data_error = temp_double{2};
             %% check if the ranging is correct
@@ -43,7 +43,7 @@ for anchor_id = 1:base_number
             fprintf(2, 'stuck at anchor %d: %s\n',anchor_id+2, rx_pkt_info);
             %% Generate the ranging packet again
             packet_id = mod(packet_id + 1, 255);
-            msg_array = [uint8(packet_id),uint8('g'),uint8(0),uint8(node_id),uint8(1),uint8(24), typecast(uint16(anchor_id+2),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(single(0.0),'uint8'), typecast(single(0.0),'uint8')];
+            msg_array = [uint8(packet_id),uint8('g'),uint8(0),uint8(anchor_id+2),uint8(1),uint8(24), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(uint16(0),'uint8'), typecast(single(0.0),'uint8'), typecast(single(0.0),'uint8')];
             % id, type, checksum, dest, src, length, data_int[5], data_float[2]
             msg_new = [uint8(255)];
             for i = 1 : length(msg_array)
@@ -63,7 +63,7 @@ for anchor_id = 1:base_number
             end
             %% check for time out
             tx_pkt_count = tx_pkt_count + 1;
-            if tx_pkt_count > 20
+            if tx_pkt_count > 2
                 sig(anchor_id) = inf;
                 stopasync(serial_port);
                 fclose(serial_port);

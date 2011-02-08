1e55c8c3c78e6511674dd1fe6ff7183a5fde8035
panshijiaustc@gmail.com <panshijiaustc@gmail.com@56807d80-2264-11df-ad4f-b38857f6a581>
2011-02-08 03:10:10 +0000

    ranging test in supermarket

--- branches/sugar/cluster/supermarket_exp/step1/ranging_distance_test.m
+++ branches/sugar/cluster/supermarket_exp/step1/ranging_distance_test.m
@@ -1,11 +1,13 @@
 %% Initialization
-iteration = 10;
+iteration = 100;
 base_number = 1;
-port = 'COM11';
+port = 'COM12';
 packet_id = floor(rand*254);
-node_id = 2;
+node_id = 3;
 sig = inf;
+sig_record = [];
 serial_port = serial(port,'BaudRate',38400,'DataBits',8,'Timeout', 0.5);
+success_rate = [];
 %% Open the serial port
 try
     fopen(serial_port);
@@ -14,17 +16,22 @@ catch ME
 end
 %% Main loop
 while 1
+    
+    succ = 0;
+    realx = input('real x: ');
+    realy = input('real y: '); 
     for c = 1 : iteration
-        [sig packet_id] = get_sig_from_port(packet_id, node_id, serial_port, base_number);
+        [sig packet_id] = get_sig_from_port(packet_id, serial_port, base_number);
+        sig_record = [sig_record, sig];
         if sig ~= inf
-            break;
+            succ = succ + 1;
         end
+        sig
     end
-    if sig == inf
-        fprintf('failed\n');
-    else
-        fprintf('ranging success\n');
-    end
+
+    success_rate = [success_rate; realx, realy, succ, succ/iteration];
+    fprintf('success rate: %d\n',succ/iteration);
+    save 'ranging_test_2_7_15_40.mat';
     cont = input('continue?');
     if cont == 0 
         break;

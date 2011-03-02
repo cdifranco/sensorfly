df055722bf598b04bc6d935ae6f8e0b4c057738f
panshijiaustc <panshijiaustc@gmail.com>
2011-03-02 21:57:16 +0000

    fail to commit the right sub set version

--- cluster/supermarket_exp/analysis/get_next_point.m
+++ cluster/supermarket_exp/analysis/get_next_point.m
@@ -5,7 +5,7 @@ ty = current_point(2);
 reference_compass_reading = signatures(signatures(:,1) == current_point(1) & signatures(:,2) == current_point(2), 4);
 isvalid_point = 0;
 while ~isvalid_point
-    step_len = 4 + unidrnd(3); %step length is from 0.6m to 0.9m
+    step_len = 2 + unidrnd(2); %step length is from 0.6m to 0.9m
     angle = (rand*2-1)*pi;
     new_x = round(tx + step_len * cos(angle));
     new_y = round(ty + step_len * sin(angle));
--- cluster/supermarket_exp/analysis/p2_data_process.m
+++ cluster/supermarket_exp/analysis/p2_data_process.m
@@ -9,12 +9,15 @@ reading_count = raw_reading_count/iteration;
 std_sig = zeros(reading_count, size(data, 2) + base_number);
 %% Get average data for each point
 for i = 1 : reading_count
-    std_sig(i, 1:4) = mean(data(20*(i-1)+1:20*i,1:4));
+    std_sig(i, 1:3) = mean(data(20*(i-1)+1:20*i,1:3));
+    std_sig(i, 4) = mean(data(20*(i-1) + find(data(20*(i-1)+1:20*i,4) ~= inf), 4));
     for j = 1 : base_number
-        std_sig(i, 5+(j-1)*2) = mean(data(20*(i-1)+1:20*i,4+j));
-        std_sig(i, 6+(j-1)*2) =  std(data(20*(i-1)+1:20*i,4+j));
+        std_sig(i, 5+(j-1)*2) = mean(data(20*(i-1) + find(data(20*(i-1)+1:20*i,4+j) ~= inf), 4+j));
+        std_sig(i, 6+(j-1)*2) = std(data(20*(i-1) + find(data(20*(i-1)+1:20*i,4+j) ~= inf), 4+j));
     end
 end
+
+
 
 %% Clear up
 clear raw_reading_count;

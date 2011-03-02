5c536c445ab0e37d4389446e25975a0e5ccf2649
panshijiaustc@gmail.com <panshijiaustc@gmail.com@56807d80-2264-11df-ad4f-b38857f6a581>
2011-03-02 19:28:18 +0000

    wrong unit for possibility

--- branches/sugar/cluster/supermarket_exp/analysis/possibility.m
+++ branches/sugar/cluster/supermarket_exp/analysis/possibility.m
@@ -1,2 +1,3 @@
 function p = possibility(d,distributionTable)
-p = interp1(distributionTable(:,1), distributionTable(:,4), d);
+d_in_meter = 0.3048 * d;
+p = interp1(distributionTable(:,1), distributionTable(:,4), d_in_meter);

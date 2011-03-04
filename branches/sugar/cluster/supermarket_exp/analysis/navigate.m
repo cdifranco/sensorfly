05cb64de0927b01cd9288138a309e1f9b5873b85
panshijiaustc@gmail.com <panshijiaustc@gmail.com@56807d80-2264-11df-ad4f-b38857f6a581>
2011-03-04 18:58:49 +0000

    testing change

--- branches/sugar/cluster/supermarket_exp/analysis/navigate.m
+++ branches/sugar/cluster/supermarket_exp/analysis/navigate.m
@@ -1,68 +1,44 @@
-function [succ sig_route error] = navigate(current_point, dest_area, base_number, trans_history, center_sig, count_to_id, matrix, area_cluster_relation, signatures, distribution_table, test_type)
-%% Initialization
-step_threshold = 500;
+function [succ sigRoute clusterRoute coordRoute startCluster destCluster] = navigate(startCoord, destCoord, stepLength, baseNumber, transHistory, centers, points, matrix, std_sig, distribution_table)
 succ = 1;
-error = 0;
-total_count = 0;
-dir_number = size(trans_history, 2);
-current_sig = signatures(signatures(:,1)==current_point(1) & signatures(:,2)==current_point(2), 5:2:end);
-total_count = total_count + 1;
-sig_route(total_count, 1:base_number) = current_sig;
-current_cluster = get_cluster_sig(center_sig, current_sig, base_number, distribution_table{5});
-dest_cluster = [];
-%% Get dest clusters
-clusters_in_area = area_cluster_relation{dest_area};
-[max_cluster_id max_cluster_number] = mode(clusters_in_area);
-if max_cluster_number < 20
-    dest_cluster = clusters_in_area(1:20);
-else 
-    while max_cluster_number >= 20
-        dest_cluster = [dest_cluster max_cluster_id];
-        clusters_in_area(clusters_in_area(:)==max_cluster_id) = [];
-        [max_cluster_id max_cluster_number] = mode(clusters_in_area);  
-    end
-end
-current_area = get_area_id(current_point);
-fprintf('start from cluster : %d to cluster : %d\n',current_cluster, dest_cluster(1));
-fprintf('start from area : %d to area : %d\n',current_area, dest_area);
-fprintf('a: %d',current_area);
-%% Main while loop
+successCnt = 0;
+totalCnt = 0;
+directionNumber = size(transHistory, 2);
+currentCoord = startCoord;
+currentSig = std_sig(std_sig(:,1) == startCoord(1) & std_sig(:,2) == startCoord(2), 5:2:end)
+baseNumber
+totalCnt = totalCnt + 1;
+sigRoute(totalCnt, 1:baseNumber) = currentSig;
+coordRoute(totalCnt, 1:2) = currentCoord;
+currentCluster = get_cluster_sig(centers, currentSig, baseNumber, distribution_table);
+startCluster = currentCluster;
+clusterRoute(totalCnt) = currentCluster;
+
+destSig = std_sig(std_sig(:,1) == destCoord(1) & std_sig(:,2) == destCoord(2), 5:end);
+destCluster = get_cluster(centers, destSig);
+
 while 1
-    %%
-    if  ~isempty(find(ismember(dest_cluster, current_cluster), 1))
-        fprintf('u r in the destination area %d\n', dest_area);
-        current_area = get_area_id(current_point);
-        dest_location = get_area_location(dest_area);
-        current_location = get_area_location(current_area);
-        error =  sqrt(sum((dest_location(:) - current_location(:)) .^ 2));
-        fprintf('the actual area %d\n', current_area);
+    if get_cluster(centers, currentSig) == destCluster
         break;
     else
-        %% Get direction
-        [path direction_order] = guide(current_cluster, dest_cluster(1), trans_history, count_to_id, matrix, test_type);
+        [path direction] = guide(currentSig, destCluster, transHistory, centers, matrix);
         if isempty(path)
-            fprintf('random :  ');
-            suggest_dir = double(unidrnd(dir_number));
+            rand_direction = ceil(directionNumber*rand);
+            currentCoord = generate_next_step(rand_direction, stepLength, currentCoord(1),currentCoord(2),points);
         else
-            suggest_dir = double(direction_order(1));
+            currentCoord = generate_next_step(direction(1), stepLength, currentCoord(1),currentCoord(2),points);
         end
-        fprintf(' --%d--> ', suggest_dir);
-        %% Go based on command
-        current_point = next_step(current_point, suggest_dir, signatures);
-        current_area = get_area_id(current_point);
-        fprintf('area: %d, cluster %d, location: (%d , %d) path: ', current_area, current_cluster, current_point(1), current_point(2));
-        for pc = 1 : size(path, 2)
-            fprintf('%d  ', path(pc));
-        end
-        fprintf('\n');
-        current_sig = signatures(signatures(:,1) == current_point(1) & signatures(:,2) == current_point(2), 5:2:end);
-        total_count = total_count + 1;
-        if total_count > step_threshold
+        totalCnt = totalCnt + 1;
+        if totalCnt > 300
             succ = 0;
             break;
         end
-        sig_route(total_count, 1:base_number) = current_sig;
-        current_cluster = get_cluster_sig(center_sig, current_sig, base_number, distribution_table{5});
-        %cont = input('cont\n');
+        currentSig = std_sig(std_sig(:,1) == convert(1) & std_sig(:,2) == convert(2), 5:end);
+        sigRoute(totalCnt, 1:baseNumber) = currentSig;
+        coordRoute(totalCnt, 1:2) = currentCoord;
+        currentCluster = get_cluster(centers, currentSig);
+        clusterRoute(totalCnt) = currentCluster;
+        if length(path) >= 1 && currentCluster == path(2)
+            successCnt = successCnt + 1;
+        end
     end
 end

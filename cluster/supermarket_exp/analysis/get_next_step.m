846c0e3c33a4bc54a2891c955deeef9b1fe0ed9a
panshijiaustc <panshijiaustc@gmail.com>
2011-03-04 19:28:46 +0000

    testing

---
+++ cluster/supermarket_exp/analysis/get_next_step.m
@@ -0,0 +1,17 @@
+% used for generate next step in testing
+function [new_x new_y] = get_next_step(direction, current_point, signatures)
+tx = current_point(1);
+ty = current_point(2);
+angle = angle_convert(direction);
+isvalid_point = 0;
+while ~isvalid_point
+    step_len = 2 + unidrnd(2); %step length is from 0.6m to 0.9m
+    new_x = round(tx + step_len * cos(angle));
+    new_y = round(ty + step_len * sin(angle));
+    if isempty(find(signatures(:,1)==new_x & signatures(:,2) == new_y, 1))
+        %fprintf('failed to generate: %d, %d\n',new_x,new_y);
+        continue;
+    else
+        isvalid_point = 1;
+    end
+end

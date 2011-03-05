159b255cd56d84b5ae635d5b14e95e9068430572
panshijiaustc <panshijiaustc@gmail.com>
2011-03-05 02:30:33 +0000

    still got problems with get next step

--- cluster/supermarket_exp/analysis/guide.m
+++ cluster/supermarket_exp/analysis/guide.m
@@ -1,25 +1,23 @@
-function [path direction_order direction_count total_cost] = guide(current_cluster, dest_cluster, trans_history, count_to_id, matrix, test_type)
+function [path direction totalCost] = guide(currentSig, destCluster, transHistory, centers, matrix, base_number, distribution_table)
 
-number_of_center  = size(matrix,1);
-direction_count = [];
-direction_order = [];
-if dest_cluster > number_of_center
+noOfNodes  = size(transHistory,1);
+
+if destCluster > noOfNodes
     error('Point number is too large.');
 else
-    farthestPreviousHop = zeros(1, number_of_center);
-    farthestNextHop = zeros(1, number_of_center);
+    farthestPreviousHop = zeros(1, noOfNodes);
+    farthestNextHop = zeros(1, noOfNodes);
     % initialize the farthest node to be itself;
-    farthestPreviousHop(1:number_of_center) = 1:number_of_center;     
-    farthestNextHop(1:number_of_center) = 1:number_of_center;
+    farthestPreviousHop(1:noOfNodes) = 1:noOfNodes;     % used to compute the RTS/CTS range;
+    farthestNextHop(1:noOfNodes) = 1:noOfNodes;
 
-    startPoint = current_cluster;
-    endPoint = dest_cluster;
-    [path, total_cost] = dijkstra(number_of_center, matrix, startPoint, endPoint, farthestPreviousHop, farthestNextHop);
-    if ~isempty(path)
-        if test_type == 1 || test_type == 2
-            [direction_count direction_order] = sort(trans_history(count_to_id(path(1)), :, count_to_id(path(2))),'descend');
-        else
-            [direction_count direction_order] = sort(trans_history(path(1), :, path(2)),'descend');
-        end
+    startPoint = get_cluster_sig(centers, currentSig, base_number, distribution_table);
+    endPoint = destCluster;
+    [path, totalCost] = dijkstra(noOfNodes, matrix, startPoint, endPoint, farthestPreviousHop, farthestNextHop);
+    
+    % get direction
+    direction = zeros(1, length(path) - 1);
+    for i = 1:length(path) - 1
+        [~, direction(i)] = max(transHistory(path(i), :, path(i+1)));
     end
 end
--- cluster/supermarket_exp/analysis/navigate.m
+++ cluster/supermarket_exp/analysis/navigate.m
@@ -1,4 +1,4 @@
-function [succ sigRoute clusterRoute coordRoute startCluster destCluster] = navigate(startCoord, destCoord, stepLength, baseNumber, transHistory, centers, points, matrix, std_sig, distribution_table, distribution_table_id)
+function [succ sigRoute clusterRoute coordRoute startCluster destCluster] = navigate(startCoord, destCoord, baseNumber, transHistory, centers, points, matrix, std_sig, distribution_table, base_number)
 succ = 1;
 successCnt = 0;
 totalCnt = 0;
@@ -8,33 +8,35 @@ currentSig = std_sig(std_sig(:,1) == startCoord(1) & std_sig(:,2) == startCoord(
 totalCnt = totalCnt + 1;
 sigRoute(totalCnt, 1:baseNumber) = currentSig;
 coordRoute(totalCnt, 1:2) = currentCoord;
-currentCluster = get_cluster_sig(centers, currentSig, baseNumber, distribution_table{distribution_table_id});
+currentCluster = get_cluster_sig(centers, currentSig, baseNumber, distribution_table);
 startCluster = currentCluster;
 clusterRoute(totalCnt) = currentCluster;
 
-destSig = std_sig(std_sig(:,1) == destCoord(1) & std_sig(:,2) == destCoord(2), 5:end);
-destCluster = get_cluster_sig(centers, destSig, baseNumber, distribution_table{distribution_table_id});
+destSig = std_sig(std_sig(:,1) == destCoord(1) & std_sig(:,2) == destCoord(2), 5:2:end);
+destCluster = get_cluster_sig(centers, destSig, baseNumber, distribution_table);
 
 while 1
-    if get_cluster_sig(centers, currentSig, baseNumber, distribution_table{distribution_table_id}) == destCluster
+    if get_cluster_sig(centers, currentSig, baseNumber, distribution_table) == destCluster
         break;
     else
-        [path direction] = guide(currentSig, destCluster, transHistory, centers, matrix);
+        [path direction] = guide(currentSig, destCluster, transHistory, centers, matrix, base_number, distribution_table);
+        path
+        direction
         if isempty(path)
             rand_direction = ceil(directionNumber*rand);
-            currentCoord = get_next_step(rand_direction, stepLength, currentCoord(1),currentCoord(2),points);
+            currentCoord = get_next_step(rand_direction, currentCoord, points);
         else
-            currentCoord = get_next_step(direction(1), stepLength, currentCoord(1),currentCoord(2),points);
+            currentCoord = get_next_step(direction(1), currentCoord, points);
         end
         totalCnt = totalCnt + 1;
         if totalCnt > 300
             succ = 0;
             break;
         end
-        currentSig = std_sig(std_sig(:,1) == convert(1) & std_sig(:,2) == convert(2), 5:end);
+        currentSig = std_sig(std_sig(:,1) == currentCoord(1) & std_sig(:,2) == currentCoord(2), 5:2:end);
         sigRoute(totalCnt, 1:baseNumber) = currentSig;
         coordRoute(totalCnt, 1:2) = currentCoord;
-        currentCluster = get_cluster_sig(centers, currentSig, baseNumber, distribution_table{distribution_table_id});
+        currentCluster = get_cluster_sig(centers, currentSig, baseNumber, distribution_table);
         clusterRoute(totalCnt) = currentCluster;
         if length(path) >= 1 && currentCluster == path(2)
             successCnt = successCnt + 1;

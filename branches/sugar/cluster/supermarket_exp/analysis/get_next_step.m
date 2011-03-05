29f2ee510dbe0075fea1f466c57befa21b187d25
panshijiaustc@gmail.com <panshijiaustc@gmail.com@56807d80-2264-11df-ad4f-b38857f6a581>
2011-03-05 05:35:57 +0000

    fixed testing

--- branches/sugar/cluster/supermarket_exp/analysis/get_next_step.m
+++ branches/sugar/cluster/supermarket_exp/analysis/get_next_step.m
@@ -1,18 +1,23 @@
 % used for generate next step in testing
-function new_coord = get_next_step(direction, current_point, signatures)
+function new_coord = get_next_step(direction, current_point, points)
 tx = current_point(1);
 ty = current_point(2);
 angle = angle_convert(direction);
-isvalid_point = 0;
-while ~isvalid_point
-    step_len = 1 + unidrnd(3); % step length is from (1~2)*2  
-    new_x = tx + round(step_len * cos(angle*pi/180));
-    new_y = ty + round(step_len * sin(angle*pi/180));
-    new_coord = [new_x new_y];
-    if isempty(find(signatures(:,1)==new_x & signatures(:,2) == new_y, 1))
-        fprintf('failed to generate: %d, %d\n',new_x,new_y);
-        continue;
-    else
-        isvalid_point = 1;
+step_len = 1 + unidrnd(3); % step length is from 1~4  
+new_x = tx + round(step_len * cos(angle*pi/180));
+new_y = ty + round(step_len * sin(angle*pi/180));
+sentry_distance = inf;
+%% project location to grid
+for i = tx - step_len : tx + step_len
+    for j = ty - step_len : ty + step_len
+        if isempty(find(points(:,1)== i & points(:,2) == j, 1))
+            continue; % check if the point has reading
+        else
+            distance = sqrt(sum(([new_x, new_y] - [i, j]) .^ 2));
+            if distance < sentry_distance
+                sentry_distance = distance;
+                new_coord = [i, j];
+            end
+        end
     end
 end

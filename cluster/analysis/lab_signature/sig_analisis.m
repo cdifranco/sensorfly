ec867d35503343c49d92f796577f78083c339503
panshijiaustc <panshijiaustc@gmail.com>
2011-03-15 17:41:53 +0000

    March 14th

--- cluster/analysis/lab_signature/sig_analisis.m
+++ cluster/analysis/lab_signature/sig_analisis.m
@@ -1,5 +1,5 @@
 close all;
-load 'raw_data_3_8.mat';
+load 'raw_data_3_14.mat';
 reading_test_result = reading;
 %% Find Valid Reading
 for c = 1 : size(reading_test_result, 1)
@@ -7,11 +7,12 @@ for c = 1 : size(reading_test_result, 1)
         valid_reading(c, :) = reading_test_result(c, :);
     end
 end
+
 %% Node 21
 node_21_reading = valid_reading(valid_reading(:,2)== 21, :);
 node_21_timestamp = node_21_reading(:, 1);
 node_21_reading_compass = node_21_reading(:, 3);
-node_21_reading_sig_2 = node_21_reading(:,10);
+node_21_reading_sig_2 = node_21_reading(:,12);
 node_21_reading_edist = zeros(1, size(node_21_reading, 1));
 for c = 1 : size(node_21_reading, 1);
     node_21_reading_edist(c) = sqrt(sum(node_21_reading(c, 4:end).*node_21_reading(c, 4:end)));
@@ -27,6 +28,7 @@ figure;
 plot(node_21_timestamp, node_21_reading_edist);
 axis([min(node_21_timestamp), max(node_21_timestamp), 0, max(node_21_reading_edist)]);
 datetick;
+%{
 %% Node 22
 node_22_reading = valid_reading(valid_reading(:,2)== 22, :);
 node_22_timestamp = node_22_reading(:, 1);
@@ -48,7 +50,8 @@ figure;
 plot(node_22_timestamp, node_22_reading_edist);
 axis([min(node_22_timestamp), max(node_22_timestamp), 0, max(node_22_reading_edist)]);
 datetick;
-%% Node 21
+
+%% Node 23
 node_23_reading = valid_reading(valid_reading(:,2)== 23, :);
 node_23_timestamp = node_23_reading(:, 1);
 node_23_reading_compass = node_23_reading(:, 3);
@@ -69,6 +72,7 @@ figure;
 plot(node_23_timestamp, node_23_reading_edist);
 axis([min(node_23_timestamp), max(node_23_timestamp), 0, max(node_23_reading_edist)]);
 datetick;
+
 %% Node 24
 node_24_reading = valid_reading(valid_reading(:,2)== 24, :);
 node_24_timestamp = node_24_reading(:, 1);
@@ -90,11 +94,12 @@ figure;
 plot(node_24_timestamp, node_24_reading_edist);
 axis([min(node_24_timestamp), max(node_24_timestamp), 0, max(node_24_reading_edist)]);
 datetick;
+
 %% Node 25
 node_25_reading = valid_reading(valid_reading(:,2)== 25, :);
 node_25_timestamp = node_25_reading(:, 1);
 node_25_reading_compass = node_25_reading(:, 3);
-node_25_reading_sig_2 = node_25_reading(:,10);
+node_25_reading_sig_2 = node_25_reading(:,13);
 node_25_reading_edist = zeros(1, size(node_25_reading, 1));
 for c = 1 : size(node_25_reading, 1);
     node_25_reading_edist(c) = sqrt(sum(node_25_reading(c, 4:end).*node_25_reading(c, 4:end)));
@@ -111,3 +116,4 @@ figure;
 plot(node_25_timestamp, node_25_reading_edist);
 axis([min(node_25_timestamp), max(node_25_timestamp), 0, max(node_25_reading_edist)]);
 datetick;
+%}

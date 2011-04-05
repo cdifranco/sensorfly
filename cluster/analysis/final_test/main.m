08e9d4d24e88f17a05f31685eaf1bb8e3dd871c2
panshijiaustc <panshijiaustc@gmail.com>
2011-04-05 09:28:39 +0000

    can be used to run hall way and lab

--- cluster/analysis/final_test/main.m
+++ cluster/analysis/final_test/main.m
@@ -1,11 +1,15 @@
 % Main
-% data(cluster_id, x, y, compass, sig)
 
-dist_error = [];
-ave_step = [];
+dist_error_collection = [];
+step_collection = [];
+save ('final_result.mat','dist_error_collection','dist_error_collection');
+clear all;
 
-for i = 4 : 10
-    kmeans;
-    training;
-    testing;
+for anchor_number = 10 : 10 % anchor number
+    for cluster_number = 5:5:30 % max cluster number
+        save ('parameters.mat', 'cluster_number', 'anchor_number');
+        kmeans_clustering;
+        training;
+        testing;
+    end
 end
